7322c5bf2aa3964356c080c2d067f2118838c352
Mohieddine Drissi <m.drissi@protonmail.com>
2026-04-07 00:48:37 +0200

    feat: add CORS middleware

--- solid/api/api.go
+++ solid/api/api.go
@@ -4,12 +4,18 @@ import (
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	v1 "github.com/zeddo123/mlsolid/solid/api/v1"
 	"github.com/zeddo123/mlsolid/solid/controllers"
 )
 
 func NewAPI(ctrl *controllers.Controller) *fiber.App {
 	app := fiber.New(fiber.Config{})
+
+	app.Use(logger.New())
+
+	app.Use(cors.New())
 
 	// Inject controller into fiber's context
 	app.Use(func(ctx *fiber.Ctx) error {
